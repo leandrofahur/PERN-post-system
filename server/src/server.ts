61a1718af4b80b2d17090696c2d27ef3643e4ebd
Leandro <fahurleandro@gmail.com>
2021-06-03 02:14:06 -0700

    first migration configured

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,14 @@
+import "reflect-metadata";
+import "./database";
 import express from "express";
 const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    status: 200,
+    message: "Hello from express with typescript!",
+  });
+});
 
 app.listen(5000, () => {
   console.log(`Server up and running on port 5000`);
